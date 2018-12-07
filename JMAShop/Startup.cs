8415ba7023eeb231988017beaab71fe38bb4895b
Jassar Al_Akhars <jalakhras@blessedtreeit.com>
2018-12-08 00:25:23 +0200

    Add configuration project

--- JMAShop/Startup.cs
+++ JMAShop/Startup.cs
@@ -15,20 +15,16 @@ namespace JMAShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDeveloperExceptionPage();//to add support to showing Exption in browser
+            app.UseStatusCodePages();//to allow handel respons status code between 400 and 600
+            app.UseStaticFiles();//to serve static File
+            app.UseMvcWithDefaultRoute();
         }
     }
 }
