43b8be59918f6fcfce610c8216919c3bb99f5016
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-01-12 02:40:17 +0200

    Creating a Custom Validation Attribute

--- JMAShop/Models/Item.cs
+++ JMAShop/Models/Item.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using JMAShop.Utility;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,12 +10,18 @@ namespace JMAShop.Models
     public class Item
     {
         public int ItemId { get; set; }
+        [MaxLength(100)]
+        [Required(ErrorMessage ="name is Requrid")]
         public string Name { get; set; }
+        [MaxLength(100)]
+        [Required(ErrorMessage = "ShortDescription is Requrid")]
         public string ShortDescription { get; set; }
         public string LongDescription { get; set; }
         public string AllergyInformation { get; set; }
         public decimal Price { get; set; }
+        [ValidUrl(ErrorMessage = "That's not a valid URL")]
         public string ImageUrl { get; set; }
+        [ValidUrl(ErrorMessage = "That's not a valid URL")]
         public string ImageThumbnailUrl { get; set; }
         public bool IsItemOfTheWeek { get; set; }
         public bool InStock { get; set; }
