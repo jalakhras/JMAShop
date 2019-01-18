3061b4b02898fb9f6d90b18fc9c605838c363774
Jassar Al-akhras <JAlakhras@blessedtreeit.com>
2019-01-19 00:24:22 +0200

    Working with Azure Application Insights

--- JMAShop/Controllers/HomeController.cs
+++ JMAShop/Controllers/HomeController.cs
@@ -2,8 +2,11 @@
 using JMAShop.Models;
 using JMAShop.Utility;
 using JMAShop.ViewModels;
+using Microsoft.ApplicationInsights;
+using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using System;
 
 namespace JMAShop.Controllers
 {
@@ -26,6 +29,11 @@ namespace JMAShop.Controllers
         [Route("Index")]
         public ViewResult Index()
         {
+            //Application Insights
+            TelemetryClient tc = new TelemetryClient();
+            tc.TrackPageView(
+                new PageViewTelemetry("Insights: JMAShop Home page loaded") { Timestamp = DateTime.UtcNow });
+            tc.TrackEvent("HomeControllerLoad");
             //Logging
             _logger.LogInformation(LogEventIds.LoadHomepage, "Loading home page");
             var homeViewModel = new HomeViewModel
--- JMAShop/Program.cs
+++ JMAShop/Program.cs
@@ -12,6 +12,7 @@ namespace JMAShop
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
+                .UseApplicationInsights()
                 .UseStartup<Startup>();
     }
 }
